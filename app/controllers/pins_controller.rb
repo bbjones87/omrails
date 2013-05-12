87d7160ca81ed7a45530d89278b80ad07a194db6
Brandon Bernard Jones <brandon.bernard.jones@gmail.com>
2013-05-12 15:36:11 -0400

    Add Paperclip Gem

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,7 +3,7 @@ class PinsController < ApplicationController
   # GET /pins
   # GET /pins.json
   def index
-    @pins = Pins.all
+    @pins = Pin.all
 
     respond_to do |format|
       format.html # index.html.erb
